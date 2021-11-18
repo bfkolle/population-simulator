8a210cc7ea3fe8cd806b782b3cc7ab3a4b18be53
Brandon Kolle <bfkolle@gmail.com>
2021-11-18 18:46:24 -0500

    Added basic support for data fetching

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,21 @@
 <template>
-	<h1>Hello world!</h1>
+	<div>
+		<div>{{ post }}</div>
+		<button @click="refresh">Refresh</button>
+	</div>
 </template>
+
+<script>
+	export default {
+		async asyncData(context) {
+			const post = await context.app.$http.$get("http://localhost:8080/all")
+			return { post }
+		},
+		methods: {
+			refresh() {
+				this.$nuxt.refresh()
+			}
+		}
+	}
+</script>
+
