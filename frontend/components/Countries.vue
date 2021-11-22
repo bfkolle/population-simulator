fd7fc7f9a1171c8351203cfeca1fac6f8f09ac05
Brandon Kolle <bfkolle@gmail.com>
2021-11-21 21:40:17 -0500

    Implemented searchable drop down menu

--- frontend/components/Countries.vue
+++ frontend/components/Countries.vue
@@ -1,24 +1,57 @@
+<!-- Design originally from https://ctnicholas.medium.com/create-a-searchable-dropdown-list-in-vue-js-139dd064e845 -->
+
 <template>
-    <div>
-        <ul>
-            <li v-for="country in countries" :key="country.name">
-				<div>{{ country }}</div>
-			</li>
-        </ul>
-        <button @click="$fetch">Refresh</button>
-    </div>
+	<div class="dropdown">
+		<input v-if="Object.keys(selectedCountry).length === 0" ref = "dropdowninput" v-model.trim="inputValue" class="dropdown-input" type="text" placeholder="Search countries" />
+		<div v-else @click="resetCountry" class="dropdown-selected">
+			{{ selectedCountry.name }}
+		</div>
+		<div v-show="inputValue" class="dropdown-list">
+			<div v-show="countryVisible(country)" v-for="country in countries.payload" :key="country.name"  @click="selectCountry(country)" class="dropdown-item">
+				{{ country.name }}
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				countries: []
+				countries: [],
+				inputValue: '',
+				selectedCountry: {}
 			}
 		},
 		async fetch() {
-			this.countries = await this.$http.$get("/tick")
-			this.countries = await this.$http.$get("/country?name=greece")
+			this.countries = await this.$http.$get("/all")
+		},
+		methods: {
+			countryVisible(country) {
+				let currentName = country.name.toLowerCase()
+				let currentInput = this.inputValue.toLowerCase()
+				return currentName.includes(currentInput)
+			},
+			selectCountry(country) {
+				this.selectedCountry = country
+				this.inputValue = ""
+				this.$emit('on-country-selected', country)
+			},
+			resetCountry() {
+				this.selectedCountry = {}
+				this.$nextTick( () => this.$refs.dropdowninput.focus() )
+				this.$emit('on-country-reset')
+			}
 		}
 	}
 </script>
+
+<style>
+	.dropdown {
+		position: relative
+	}
+
+	.dropdown-list {
+		position: absolute
+	}
+</style>
