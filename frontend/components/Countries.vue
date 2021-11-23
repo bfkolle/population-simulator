353418c6c8f5bc8a9faf052d274fafd23046d08a
Brandon Kolle <bfkolle@gmail.com>
2021-11-22 19:00:05 -0500

    Added selected country display

--- frontend/components/Countries.vue
+++ frontend/components/Countries.vue
@@ -1,8 +1,10 @@
-<!-- Design originally from https://ctnicholas.medium.com/create-a-searchable-dropdown-list-in-vue-js-139dd064e845 -->
+<!-- Incorporates design from https://ctnicholas.medium.com/create-a-searchable-dropdown-list-in-vue-js-139dd064e845 -->
 
 <template>
 	<div class="dropdown">
-		<input v-if="Object.keys(selectedCountry).length === 0" ref = "dropdowninput" v-model.trim="inputValue" class="dropdown-input" type="text" placeholder="Search countries" />
+		<h1>Population Simulator</h1>
+
+		<input v-if="isEmptyObject(selectedCountry)" ref="dropdowninput" v-model.trim="inputValue" class="dropdown-input" type="text" placeholder="Search countries" />
 		<div v-else @click="resetCountry" class="dropdown-selected">
 			{{ selectedCountry.name }}
 		</div>
@@ -10,6 +12,16 @@
 			<div v-show="countryVisible(country)" v-for="country in countries.payload" :key="country.name"  @click="selectCountry(country)" class="dropdown-item">
 				{{ country.name }}
 			</div>
+		</div>
+
+		<div v-if="!isEmptyObject(selectedCountry)">
+			<h2>{{ selectedCountry.name }}</h2>
+			<ul>
+				<li>Population: {{ selectedCountry.population.toLocaleString() }}</li>
+				<li>Population Growth Rate: {{ selectedCountry.populationGrowthRate * 100 }}%</li>
+				<li>GDP Per Capita: ${{ getGdpPercapita(selectedCountry).toLocaleString('en-US', { maximumFractionDigits: 2 }) }}</li>
+				<li>GDP Growth Rate: {{ selectedCountry.economy.gdpGrowthRate * 100 }}%</li>
+			</ul>
 		</div>
 	</div>
 </template>
@@ -35,12 +47,17 @@
 			selectCountry(country) {
 				this.selectedCountry = country
 				this.inputValue = ""
-				this.$emit('on-country-selected', country)
 			},
 			resetCountry() {
 				this.selectedCountry = {}
 				this.$nextTick( () => this.$refs.dropdowninput.focus() )
-				this.$emit('on-country-reset')
+			},
+			isEmptyObject(object) {
+				return Object.keys(object).length === 0
+			},
+			getGdpPercapita(country) {
+				let economy = country.economy
+				return economy.gdp / country.population
 			}
 		}
 	}
